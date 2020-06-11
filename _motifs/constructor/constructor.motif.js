89306b6a5f9a2b964e6f8ea367ed70de41176044
Romaric Ruga <romaric.ruga@univ-reims.fr>
2020-06-11 21:20:24 +0200

    pre v-2

--- _motifs/constructor/constructor.motif.js
+++ _motifs/constructor/constructor.motif.js
@@ -5,7 +5,23 @@
 module.exports = {
   id: 'constructor',
   name: 'Constructor',
+  description: 
+`
+\`\`\`js
+constructor = (motif, params) => instance
+\`\`\`
+
+\`constructor\` is a *specialization* of \`processor\`
+(\`(instance, params) => any // and could have side effects\`) with :
+* \`instance\`'s pattern set to \`motif\` and
+* \`return\` set to \`instance\`.
+
+*It is unclear for me now from which of the \`Abstract factory\`, the \`Builder\`
+or the \`Factory\` pattern the "Constructor" \`motif\` is the closer ...*
+`,
   synonyms: [
+    'Abstract factory',
+    'Builder',
     'Factory'
   ],
   commands : false
